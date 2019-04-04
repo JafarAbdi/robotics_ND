e2c2d3d11882a687f3bdbab3c46fbcc48f922a4f
lievbirman <lxb824@miami.edu>
2019-04-04 10:08:02 -0400

    wrist_center via IK and FK calcs

--- FK_1.py
+++ FK_1.py
@@ -13,39 +13,45 @@ b0,b1,b2,b3,b4,b5,b6 = symbols('b0:7')
 
 
 #DH parameters
-s = {b0:0,a0    :0,d1     :0.75,
-     b1:-pi/2,a1:0.35,d2  :0,q2:q2-pi/2,
-     b2:0,a2    :1.25,d3  :0,
-     b3:-pi/2,a3:-0.054,d4:0.96,
-     b4:pi/2,a4 :0,d5     :0.54,
-     b5:-pi/2,a5:0,d6     :0.193,
-     b6:0,a6    :0,d7     :0.11,q7:0}
+s = {b0:        0,          a0:        0,          d1:      0.75,
+     b1:    -pi/2,          a1:     0.35,          d2:         0,      q2:q2-pi/2,
+     b2:        0,          a2:     1.25,          d3:         0,
+     b3:    -pi/2,          a3:   -0.054,          d4:       1.5,
+     b4:     pi/2,          a4:        0,          d5:         0,
+     b5:    -pi/2,          a5:        0,          d6:         0,
+     b6:        0,          a6:        0,          d7:     0.303,      q7:0
+     }
 
-def DH_T(ap,th,a,d):
+def DH_T(ap,a,th,d):
 
-    R_x = Matrix([[ 1, 0, 0, 0],
-                  [ 0, cos(ap), -sin(ap), 0],
-                  [ 0, sin(ap), cos(ap), 0],
-                  [0, 0, 0, 1]])
+    R_x = Matrix([[       1,       0,        0,        0],
+                  [       0, cos(ap), -sin(ap),        0],
+                  [       0, sin(ap),  cos(ap),        0],
+                  [       0,        0,        0,       1]])
 
-    R_z = Matrix([[ cos(th), -sin(th), 0,0],
-                  [ sin(th), cos(th), 0,0],
-                  [ 0, 0, 1,0],
-                  [0, 0, 0, 1]])
+    R_z = Matrix([[ cos(th), -sin(th),        0,       0],
+                  [ sin(th),  cos(th),        0,       0],
+                  [       0,        0,        1,       0],
+                  [       0,        0,        0,       1]])
 
     TX = Matrix([[1, 0, 0, a],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1]])
+                 [0, 1, 0, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 0, 1]])
 
     TZ = Matrix([[1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, d],
-                [0, 0, 0, 1]])
+                 [0, 1, 0, 0],
+                 [0, 0, 1, d],
+                 [0, 0, 0, 1]])
 
-    DH_T = Matrix(R_x*TX*R_z*TZ)
+    DH_T = Matrix(R_x* TX * R_z * TZ)
 
     return DH_T
+
+# [cos(th)        ,-sin(th)          ,0         ,a         ]
+# [sin(th)*cos(ap),cos(ap)*cos(th)   ,-sin(ap)  ,-d*sin(ap)]
+# [sin(ap)*sin(th),sin(ap)*cos(th)   ,cos(ap)   ,d*cos(ap) ]
+# [0              ,0                 ,0         ,1         ]
 
 # DH-type transform
 # i-1 to i -> R_x( alpha i-1 )*TX( a i-1 )*R_z( theta i )*TZ( d i )
@@ -65,6 +71,49 @@ T5_6 = T5_6.subs(s)
 T6_G = DH_T(b6,a6,q7,d7)
 T6_G = T6_G.subs(s)
 
-T_total = simplify(T0_1 * T1_2  * T2_3 * T3_4 * T4_5 * T5_6 * T6_G)
+T0_G = simplify(T0_1 * T1_2  * T2_3 * T3_4 * T4_5 * T5_6 * T6_G)
 
-print(T_total)
+# R_z = Matrix([[ cos(pi), -sin(pi), 0,0],
+#               [ sin(pi), cos(pi), 0,0],
+#               [ 0, 0, 1,0],
+#               [0, 0, 0, 1]])
+# R_y = Matrix([[cos(-pi/2), 0,sin(-pi/2),0],
+#               [ 0,1, 0,0],
+#               [-sin(-pi/2), 0, cos(-pi/2),0],
+#               [0, 0, 0, 1]])
+#
+# R_corr = simplify(R_z * R_y)
+#
+#T_total = simplify(T0_G * R_corr)
+
+T_total = simplify(T0_G)
+
+# (cos(q2) , 0       , sin(q2), 1.196*sin(q2) + 1.803*cos(q2) + 0.35)
+# (0       , 1       , 0      , 0)
+# (-sin(q2), 0       , cos(q2), -1.803*sin(q2) + 1.196*cos(q2) + 0.75)
+# (0       , 0       , 0      , 1)
+
+# Calculating Spherical Wrist Center (Origin 4,5,6)
+
+#Forward Kinematics
+wrist_center = simplify(T0_1 * T1_2 * T2_3 * T3_4)*Matrix([[0],[0],[0],[1]])
+wrist_center = wrist_center.evalf(subs={q2:0})
+print(wrist_center)
+
+#Inverse Kinematics
+R0_6 = T_total[0:3,0:3]
+p = T_total[0:3,3:4]
+i = Matrix([[0],[0],[1]])
+wrist_center = p-s[d7]*R0_6*i
+wrist_center = wrist_center.evalf(subs={q2:0})
+print(wrist_center)
+
+
+# Calculating End Effector Position
+end_effector = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G)*Matrix([[0],[0],[0],[1]])
+end_effector = end_effector.evalf(subs={q2:0})
+#print(end_effector)
+
+# End Effector Relative to Wrist Center
+# ee_wrt_wc = end_effector-wrist_center
+#print(ee_wrt_wc)
