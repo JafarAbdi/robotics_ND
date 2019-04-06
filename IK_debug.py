81d24b92a0ca1f6dc57f203c95ba6795392c9b19
lievbirman <lxb824@miami.edu>
2019-04-06 13:38:36 -0400

    fillied in IKdebug and giving to VM

--- IK_debug.py
+++ IK_debug.py
@@ -41,51 +41,166 @@ def test_code(test_case):
             self.y = EE_ori[1]
             self.z = EE_ori[2]
             self.w = EE_ori[3]
-
     position = Position(test_case[0][0])
     orientation = Orientation(test_case[0][1])
-
     class Combine:
         def __init__(self,position,orientation):
             self.position = position
             self.orientation = orientation
-
     comb = Combine(position,orientation)
-
     class Pose:
         def __init__(self,comb):
             self.poses = [comb]
-
     req = Pose(comb)
     start_time = time()
-    
     ########################################################################################
-    ## 
-
     ## Insert IK code here!
-    
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
-    theta4 = 0
-    theta5 = 0
-    theta6 = 0
 
-    ## 
+    ###joint variables
+    q1,q2,q3,q4,q5,q6,q7 = symbols('q1:8')
+    d1,d2,d3,d4,d5,d6,d7 = symbols('d1:8')
+    a0,a1,a2,a3,a4,a5,a6 = symbols('a0:7')
+    b0,b1,b2,b3,b4,b5,b6 = symbols('b0:7')
+
+    #DH parameters
+    s = {b0:        0,          a0:        0,          d1:      0.75,      q1:q1,
+         b1:    -pi/2,          a1:     0.35,          d2:         0,      q2:q2-pi/2,
+         b2:        0,          a2:     1.25,          d3:         0,      q3:q3,
+         b3:    -pi/2,          a3:   -0.054,          d4:       1.5,      q4:q4,
+         b4:     pi/2,          a4:        0,          d5:         0,      q5:q5,
+         b5:    -pi/2,          a5:        0,          d6:         0,      q6:q6,
+         b6:        0,          a6:        0,          d7:     0.303,      q7:0
+         }
+
+    #defining transformation matrix function
+    th, ap, a, d = symbols('th ap a d')
+    def DH_T(ap,a,th,d):
+
+        R_x = Matrix([[       1,       0,        0,        0],
+                      [       0, cos(ap), -sin(ap),        0],
+                      [       0, sin(ap),  cos(ap),        0],
+                      [       0,        0,        0,       1]])
+
+        R_z = Matrix([[ cos(th), -sin(th),        0,       0],
+                      [ sin(th),  cos(th),        0,       0],
+                      [       0,        0,        1,       0],
+                      [       0,        0,        0,       1]])
+
+        TX = Matrix([[1, 0, 0, a],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
+
+        TZ = Matrix([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, d],
+                     [0, 0, 0, 1]])
+
+        DH_T = Matrix(R_x* TX * R_z * TZ)
+
+        return DH_T
+
+    #creating individual transforms
+    T0_1 = DH_T(b0,a0,q1,d1).subs(s)
+    T1_2 = DH_T(b1,a1,q2,d2).subs(s)
+    T2_3 = DH_T(b2,a2,q3,d3).subs(s)
+    T3_4 = DH_T(b3,a3,q4,d4).subs(s)
+    T4_5 = DH_T(b4,a4,q5,d5).subs(s)
+    T5_6 = DH_T(b5,a5,q6,d6).subs(s)
+    T6_EE = DH_T(b6,a6,q7,d7).subs(s)
+
+    T0_G = simplify(T0_1 * T1_2  * T2_3 * T3_4 * T4_5 * T5_6 * T6EE)
+
+    #Extract end-effector position and orientation
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+        [req.poses[x].orientation.x, req.poses[x].orientation.y,
+            req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+    #rotation error
+    R_z = Matrix([[ cos(pi), -sin(pi), 0,0],
+                  [ sin(pi), cos(pi), 0,0],
+                  [ 0, 0, 1,0],
+                  [0, 0, 0, 1]])
+    R_y = Matrix([[cos(-pi/2), 0,sin(-pi/2),0],
+                  [ 0,1, 0,0],
+                  [-sin(-pi/2), 0, cos(-pi/2),0],
+                  [0, 0, 0, 1]])
+
+    R_corr = simplify(R_z * R_y)
+
+
+    #find EE rotation matrix
+    r,p,y = symbols('r p y')
+
+    RX = Matrix([[       1,      0,       0],
+                 [       0, cos(r), -sin(r)],
+                 [       0, sin(r),  cos(r)]])
+
+    RY = Matrix([[  cos(p),-sin(p),       0],
+                 [  sin(p), cos(p),       0],
+                 [       0,      0,       1]])
+
+    RZ = Matrix([[  cos(y),      0,  sin(y)],
+                 [       0,      1,       0],
+                 [ -sin(y),      0,  cos(y)]])
+
+    R_EE = (RZ * RY * RX) * R_corr
+    R_EE.subs({'r':roll, 'p':pitch, 'y':yaw})
+
+    EE = Matrix([[px],
+                 [py],
+                 [pz]])
+
+    WC = EE - (0.303 * R_EE[:,2])
+
+    #THETA1
+    theta1 = atan2(WC[1],WC[0])
+
+    #TRIANGLE FOR THETA 1 and 2
+    a = 1.501
+    b = sqrt(pow((sqrt(WC[0]*WC[0]+WC[1]*WC[1])-0.35) ,2) + pow((WC[2]-0.75), 2))
+    c = 1.25
+
+    A = acos((b*b + c*c - a*a)/(2*b*c))
+    B = acos((a*a + c*c - b*b)/(2*a*c))
+    C = acos((b*b + a*a - c*c)/(2*b*a))
+
+    #THETA2
+    theta2 = pi/2 - A - atan2(WC[2]-0.75, sqrt(WC[0]*WC[0] + WC[1]*WC[1])-0.35)
+
+    #THETA3
+    theta3 = pi/2 - (B + 0.036)
+
+    #
+    R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+    R0_3 = R0_3.evalf(subs={q1: theta1, q2:theta2, q3:theta3})
+    R3_6 = R0_3.inv("LU")*R_EE
+
+    #THETA4
+    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+    #THETA5
+    theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
+    #THETA6
+    theta4 = atan2(-R3_6[1,1], R3_6[1,0])
+
     ########################################################################################
-    
+
     ########################################################################################
     ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
     ## (OPTIONAL) YOUR CODE HERE!
+    FK = T0_EE.evalf(subs={q1:theta1,q2:theta2,q3:theta3,q4:theta4,q5:theta5,q6:theta6})
 
     ## End your code input for forward kinematics here!
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    your_wc = [WC[0],WC[1],WC[2]] # <--- Load your calculated WC values in this array
+    your_ee = [FK[0,3],FK[1,3],FK[2,3]] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
     ## Error analysis
